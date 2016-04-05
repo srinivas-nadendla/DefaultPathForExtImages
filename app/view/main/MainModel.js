eeeb87ed529af47a59e73a0d9a22eb9f7fcee1b5
Srinivas Nadendla <srinivas.nadendla@walkingtree.in>
2016-04-05 11:04:37 +0530

    Initial Code  Check In

---
+++ app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('Sample.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'Sample'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
